5523a37c579de2f42a0c0a0f17636fb433461c4b
Alejandro González Reyes <alexglzrys@gmail.com>
2021-10-24 17:59:20 -0500

    eliminar archivos

--- src/app/components/file-item/file-item.component.ts
+++ src/app/components/file-item/file-item.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {IFile} from "../../interfaces/IFile";
+import {FileService} from "../../services/file.service";
 
 @Component({
   selector: 'app-file-item',
@@ -9,11 +10,21 @@ import {IFile} from "../../interfaces/IFile";
 export class FileItemComponent implements OnInit {
 
   @Input() fileInfo!: IFile;
+  @ViewChild('myFile') myFileElement!: ElementRef
 
-  constructor() { }
+  constructor(private fileService: FileService) { }
 
   ngOnInit(): void {
 
+  }
+
+  delete(file: IFile) {
+    this.fileService.deleteFile(file).subscribe(response => {
+      if (response.code == 200) {
+        this.myFileElement.nativeElement.remove()
+      }
+      console.log(response)
+    })
   }
 
 }
--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {IFile} from "../interfaces/IFile";
 
@@ -13,5 +13,11 @@ export class FileService {
   getAllFiles(): Observable<IFile[]> {
     const URL_SERVER = 'http://localhost:8888/Backend/services-angular-archivos/leer-carpeta.php';
     return this.http.get<IFile[]>(URL_SERVER);
+  }
+
+  deleteFile(file: IFile): Observable<any> {
+    const URL_SERVER = 'http://localhost:8888/Backend/services-angular-archivos/borrar-archivo.php';
+    let data = {'archivo': file.ruta + '/' + file.nombre}
+    return this.http.get<any>(URL_SERVER, {params: data});
   }
 }
