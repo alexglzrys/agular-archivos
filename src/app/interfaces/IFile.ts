29fdfa40a064df03129097c23ea496f4c13a1e60
Alejandro González Reyes <alexglzrys@gmail.com>
2021-10-24 16:13:58 -0500

    agregar interfaz para información de archivo

---
+++ src/app/interfaces/IFile.ts
@@ -0,0 +1,7 @@
+export interface IFile {
+  nombre: string;
+  tipo: string;
+  peso: string;
+  ruta: string;
+  raiz: string;
+}
--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {IFile} from "../interfaces/IFile";
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class FileService {
 
   constructor(private http: HttpClient) { }
 
-  getAllFiles(): Observable<any> {
+  getAllFiles(): Observable<IFile[]> {
     const URL_SERVER = 'http://localhost:8888/Backend/services-angular-archivos/leer-carpeta.php';
-    return this.http.get<any>(URL_SERVER);
+    return this.http.get<IFile[]>(URL_SERVER);
   }
 }
